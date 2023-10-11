54fb77a31adfef7c6b395b10f7e6a2edda32b88d
SImon <33484974+simonjuha@users.noreply.github.com>
2023-10-11 12:53:48 +0200

    initial displaydriver

---
+++ include/displayDriver/displayDriver.hpp
@@ -0,0 +1,36 @@
+#include <Adafruit_SSD1306.h>
+#include <Wire.h>
+
+#define SSD1306_ADDRESS 0x3C
+#define SSD1306_SCREEN_WIDTH 128
+#define SSD1306_SCREEN_HEIGHT 64
+#define SSD1306_SDA_PIN 20
+#define SSD1306_SCL_PIN 19
+
+class DisplayDriver {
+    public:
+    DisplayDriver(){
+        Wire.begin(SSD1306_SDA_PIN, SSD1306_SCL_PIN);
+        _display = Adafruit_SSD1306(SSD1306_SCREEN_WIDTH, SSD1306_SCREEN_HEIGHT, &Wire, -1);
+        _display.begin(SSD1306_SWITCHCAPVCC, SSD1306_ADDRESS);
+        _display.setTextColor(WHITE);
+        _display.setTextSize(1);
+        _display.clearDisplay();
+    }
+    void writeLine(String inputText){
+        static int line = 0;
+        _display.setCursor(0,line);
+        _display.println(inputText);
+        _display.display();
+        line += 10;
+        if(line > 60){
+            line = 0;
+            _display.clearDisplay();
+        }
+    }
+    private:
+    Adafruit_SSD1306 _display;
+};
+
+
+
--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,19 @@
     ESP32-S3-DevKitC-1-N8R8 (8 MB)
 */
 #include <Arduino.h>
-//#include "rotaryEncoderDriver/rotaryEncoderDriver.hpp"
-#include "rotaryEncoderDriver/quardratureDriver.hpp"
-#include "booleanDriver/gpioDriver.hpp"
-// measure time lib (C++)
-#include <chrono>
 #include <RTOS.h>
 
+#include "rotaryEncoderDriver/quardratureDriver.hpp"
+#include "booleanDriver/gpioDriver.hpp"
+#include "displayDriver/displayDriver.hpp"
 
+ 
 
 class testReciver : public IRotaryObserver, public IEdgeObserver{
     public:
+        testReciver(String name){
+            _name = name;
+        }
         void up(){
             upFlag = true;
         }
@@ -27,19 +29,23 @@ class testReciver : public IRotaryObserver, public IEdgeObserver{
         }
         void tick(){
             if(upFlag){
-                Serial.println("up");
+                //display.writeLine(_name + ": up");
+                Serial.println(_name + ": up");
                 upFlag = false;
             }
             if(downFlag){
-                Serial.println("down");
+                //display.writeLine(_name + ": down");
+                Serial.println(_name + ": down");
                 downFlag = false;
             }
             if(riseFlag){
-                Serial.println("rise");
+                //display.writeLine(_name + ": rise");
+                Serial.println(_name + ": rise");
                 riseFlag = false;
             }
             if(fallFlag){
-                Serial.println("fall");
+                //display.writeLine(_name + ": fall");
+                Serial.println(_name + ": fall");
                 fallFlag = false;
             }
         }
@@ -47,31 +53,79 @@ class testReciver : public IRotaryObserver, public IEdgeObserver{
         bool fallFlag = false;
         bool upFlag   = false;
         bool downFlag = false;
+        String _name;
 };
 
 void setup() 
 {
+    
     Serial.begin(115200);
+
+
+    
      gpio_install_isr_service(ESP_INTR_FLAG_IRAM);
     xTaskCreate([](void *pvParameters){
-        testReciver t;
+        delay(1000);
+        DisplayDriver display;
+            display.writeLine("Hello World");
+    display.writeLine("new line");
+    display.writeLine("another new line");
+    display.writeLine("another new line");
+    display.writeLine("another new line");
+        testReciver t1("t1");
+        testReciver t2("t2");
         Quad::Quardrature q0(QUADRATURE0_CLK, QUADRATURE0_DT);
+        Quad::Quardrature q1(4, 5);
+       
+        
         //Quad::initQuadratureInterrupt();
-        q0.attach(&t);
+        q0.attach(&t1);
+        q0.attach(&t2);
+        q1.attach(&t1);
+        q1.attach(&t2);
+        
+ 
+
         while(1){
             vTaskDelay(10 / portTICK_PERIOD_MS);
-            t.tick();
+            if(t1.downFlag){
+                display.writeLine("t1: down");
+                t1.downFlag = false;
+            }
+            if(t1.upFlag){
+                display.writeLine("t1: up");
+                t1.upFlag = false;
+            }
+            if(t1.riseFlag){
+                display.writeLine("t1: rise");
+                t1.riseFlag = false;
+            }
+            if(t1.fallFlag){
+                display.writeLine("t1: fall");
+                t1.fallFlag = false;
+            }
+            t1.tick();
+            t2.tick();
         }
     }, "rotary", 10000, NULL, 1, NULL);
     
 
     xTaskCreate([](void *pvParameters){
         gpioDriver d = gpioDriver(17);
-        testReciver t;
-        d.attach(&t);          
+        gpioDriver d7 = gpioDriver(7);
+        testReciver t3("t3");
+        testReciver t4("t4");
+
+
+        d.attach(&t3);
+        d7.attach(&t3);
+        d.attach(&t4);
+        d7.attach(&t4);
+
         while(1){
             vTaskDelay(10 / portTICK_PERIOD_MS); 
-            t.tick();
+            t3.tick();
+            t4.tick();
         }
     }, "pushswitch", 10000, NULL, 1, NULL);
 
