8baf680bbdec1ce7b1fcef7eb78e09dc74a3909a
SImon <33484974+simonjuha@users.noreply.github.com>
2023-10-23 16:09:30 +0200

    working SD communication (MIRACLE!)

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 /*
     ESP32-S3-DevKitC-1-N8R8 (8 MB)
 */
+/*
 #include <Arduino.h>
 #include <RTOS.h>
 
@@ -9,146 +10,81 @@
 #include "displayDriver/displayDriver.hpp"
 #include "cvDriver/adcDriver.hpp"
 #include "ledDriver/ledDriver.hpp"
+#include "sdCardDriver/sdCardDriver.hpp"
+*/
 
- 
+#include "Arduino.h"
+#include <SPI.h>
+#include <SD.h>
 
-class testReciver : public IRotaryObserver, public IEdgeObserver{
-    public:
-        testReciver(String name){
-            _name = name;
-        }
-        void up(){
-            upFlag = true;
-        }
-        void down(){
-            downFlag = true;
-        }
-        void rise(){
-            riseFlag = true;
-        }
-        void fall(){
-            fallFlag = true;
-        }
-        void tick(){
-            if(upFlag){
-                //display.writeLine(_name + ": up");
-                Serial.println(_name + ": up");
-                upFlag = false;
-            }
-            if(downFlag){
-                //display.writeLine(_name + ": down");
-                Serial.println(_name + ": down");
-                downFlag = false;
-            }
-            if(riseFlag){
-                //display.writeLine(_name + ": rise");
-                Serial.println(_name + ": rise");
-                riseFlag = false;
-            }
-            if(fallFlag){
-                //display.writeLine(_name + ": fall");
-                Serial.println(_name + ": fall");
-                fallFlag = false;
-            }
-        }
-        bool riseFlag = false;
-        bool fallFlag = false;
-        bool upFlag   = false;
-        bool downFlag = false;
-        String _name;
-};
+#define SS   10
+#define MOSI 11
+#define MISO 12
+#define SCK  13
 
-void setup() 
-{
-    
+SPIClass *hspi;
+
+void setup() {
+
     Serial.begin(115200);
+    Serial.print("MOSI: ");
+    Serial.println(MOSI);
+    Serial.print("MISO: ");
+    Serial.println(MISO);
+    Serial.print("SCK: ");
+    Serial.println(SCK);
+    Serial.print("SS: ");
+    Serial.println(SS); 
 
+    pinMode(SS, OUTPUT);
+    pinMode(MISO, INPUT);
 
+    // initialize SPI
+    hspi  = new SPIClass(0);
+    hspi->begin(SCK, MISO, MOSI, SS);
     
-     gpio_install_isr_service(ESP_INTR_FLAG_IRAM);
-    xTaskCreate([](void *pvParameters){
-        delay(1000);
-        DisplayDriver display;
-            display.writeLine("Hello World");
-    display.writeLine("new line");
-    display.writeLine("another new line");
-    display.writeLine("another new line");
-    display.writeLine("another new line");
-        testReciver t1("t1");
-        testReciver t2("t2");
-        Quad::Quardrature q0(QUADRATURE0_CLK, QUADRATURE0_DT);
-        Quad::Quardrature q1(4, 5);
-       
-        
-        //Quad::initQuadratureInterrupt();
-        q0.attach(&t1);
-        q0.attach(&t2);
-        q1.attach(&t1);
-        q1.attach(&t2);
-        
-
-        adcDriver adc(ADC1_CHANNEL_7);
-        int lastRead = 0;
-
-        while(1){
-            vTaskDelay(10 / portTICK_PERIOD_MS);
-            if(t1.downFlag){
-                display.writeLine("t1: down");
-                t1.downFlag = false;
-            }
-            if(t1.upFlag){
-                display.writeLine("t1: up");
-                t1.upFlag = false;
-            }
-            if(t1.riseFlag){
-                display.writeLine("t1: rise");
-                t1.riseFlag = false;
-            }
-            if(t1.fallFlag){
-                display.writeLine("t1: fall");
-                t1.fallFlag = false;
-            }
-            t1.tick();
-            t2.tick();
-            if(lastRead > adc.read() + 100 || lastRead < adc.read() - 100){
-                display.writeLine("adc: " + String(adc.read()));
-                lastRead = adc.read();
-            }
-
-        }
-    }, "rotary", 10000, NULL, 1, NULL);
-    
-
-    xTaskCreate([](void *pvParameters){
-        gpioDriver d = gpioDriver(17);
-        gpioDriver d7 = gpioDriver(7);
-        testReciver t3("t3");
-        testReciver t4("t4");
-
-
-        d.attach(&t3);
-        d7.attach(&t3);
-        d.attach(&t4);
-        d7.attach(&t4);
-
-        LedDriver led = LedDriver(35);
-
-        while(1){
-            vTaskDelay(10 / portTICK_PERIOD_MS); 
-            if(d.read()){
-                led.set(false);
-            }else{
-                led.set(true);
-            }
-            t3.tick();
-            t4.tick();
-        }
-    }, "pushswitch", 10000, NULL, 1, NULL);
-
 }
 
+void loop() {
 
-void loop() 
-{
+    delay(1000);
+    // WORKING SPI COMMUNICATION
+    /*
+        hspi->beginTransaction(SPISettings(100000, MSBFIRST, SPI_MODE0));
+        digitalWrite(SS, LOW);
+        // send CMD0
+        hspi->transfer(0x40);
+        hspi->transfer(0x00);
+        hspi->transfer(0x00);
+        hspi->transfer(0x00);
+        hspi->transfer(0x00);
+        hspi->transfer(0x95);
+        // receive R1
+        uint8_t r1 = hspi->transfer(0xFF);
+        uint8_t r2 = hspi->transfer(0xFF);
+        digitalWrite(SS, HIGH);
+        hspi->endTransaction();
+
+        Serial.print("R1: ");
+        Serial.println(r1, HEX);
+        Serial.print("R2: ");
+        Serial.println(r2, HEX);
+    */
+
+    // SD CARD COMMMUNICATION (SD.h)
     
+        if (!SD.begin(SS, *hspi)) {
+            Serial.println("Card Mount Failed");
+            return;
+        }else{
+            Serial.println("Card Mount Success");
+            // print file names
+            File root = SD.open("/");
+            File file = root.openNextFile();
+            while(file){
+                Serial.println(file.name());
+                file = root.openNextFile();
+            }
+        }
+
 }
