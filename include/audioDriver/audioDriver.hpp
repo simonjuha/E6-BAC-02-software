7ea7d43bdf6a68b6de5183412fbf5aca1e5967b1
SImon <33484974+simonjuha@users.noreply.github.com>
2023-10-26 13:44:10 +0200

    AudioDriver working with correct sample rate (seemingly)

--- include/audioDriver/audioDriver.hpp
+++ include/audioDriver/audioDriver.hpp
@@ -3,12 +3,17 @@
 #include <SPI.h>
 #include <vector>
 
+#include <RTOS.h>
+
 #define AUDIO_SPI_CLK 4
 #define AUDIO_SPI_DAT 5
 #define AUDIO_SPI_LR  6
 #define AUDIO_SPI_BUS 1
 
 #define NUM_OF_SAMPLES 2
+
+#define SAMPLERATE 44100
+#define SAMPLE_INTERVAL 1000000/SAMPLERATE // in microseconds
 
 // this class needs to be optimized for speed as much as possible
 // BU9480F
@@ -28,18 +33,24 @@ class AudioDriver {
 
     // doesn't work in loop() ?
     void tick(){
-        digitalWrite(AUDIO_SPI_LR, HIGH);
-        vspi->transfer(sampleSlot[0]->buffer[currentPosition[0]]);
-        digitalWrite(AUDIO_SPI_LR, LOW);
-        vspi->transfer(sampleSlot[1]->buffer[currentPosition[1]]);
-        
-        currentPosition[0]++;
-        if(currentPosition[0] > this->sampleSlot[0]->size){
-            this->currentPosition[0] = 0;
-        }
-        currentPosition[1]++;
-        if(currentPosition[1] > this->sampleSlot[1]->size){
-            this->currentPosition[1] = 0;
+        static uint32_t lastSampleTime = 0;
+        uint32_t currentTime = micros();
+
+        if(currentTime - lastSampleTime >= SAMPLE_INTERVAL){
+            digitalWrite(AUDIO_SPI_LR, HIGH);
+            vspi->transfer(sampleSlot[0]->buffer[currentPosition[0]]);
+            digitalWrite(AUDIO_SPI_LR, LOW);
+            vspi->transfer(sampleSlot[1]->buffer[currentPosition[1]]);
+            
+            if(currentPosition[0]++ > this->sampleSlot[0]->size){
+                this->currentPosition[0] = 0;
+            }
+
+            if(currentPosition[1]++ > this->sampleSlot[1]->size){
+                this->currentPosition[1] = 0;
+            }
+
+            lastSampleTime = currentTime;
         }
     }
 
