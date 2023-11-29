4ce5270bc9bf332e13c7234a2b93c01b71310e92
SImon <33484974+simonjuha@users.noreply.github.com>
2023-11-29 21:22:19 +0100

    use ILed interface

--- include/ledDriver/MenuSelectionLeds.hpp
+++ include/ledDriver/MenuSelectionLeds.hpp
@@ -4,20 +4,20 @@
 class MenuSelectionLeds
 {
     public:
-    MenuSelectionLeds() : _leds {LedDriver(45), LedDriver(42), LedDriver(38), LedDriver(47), LedDriver(21)}{
+    MenuSelectionLeds() : _leds {new LedDriver(45), new LedDriver(42), new LedDriver(38), new LedDriver(47), new LedDriver(21)}{
     }
     void select(int channel){
         for(int i = 0; i < 5; i++){
             if(i == channel){
-                _leds[i].set(true);
+                _leds[i]->set(true);
             }else{
-                _leds[i].set(false);
+                _leds[i]->set(false);
             }
         }
     }
-    LedDriver* getLed(int channel){
-        return &_leds[channel];
+    ILed* getLed(int channel){
+        return _leds[channel];
     }
     private:
-    LedDriver _leds[5];
+    ILed *_leds[5];
 };
