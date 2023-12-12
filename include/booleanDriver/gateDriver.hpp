1c1ea2e96d3429d72b1808e99978ad8d6eab3597
SImon <33484974+simonjuha@users.noreply.github.com>
2023-12-12 12:33:03 +0100

    gateDriver comments

--- include/booleanDriver/gateDriver.hpp
+++ include/booleanDriver/gateDriver.hpp
@@ -8,18 +8,20 @@
 #define AUDIO_GATE_3 8
 #define AUDIO_GATE_4 18
 
+// gate inputs used by the GateInputs class
 class GateObserver : public IEdgeObserver
 {
     public:
     GateObserver(int channel) : _channel(channel)
     {
     }
-    bool risingEdge(){
+    // risingEdge: return true if edge is rising, false is nothing happened 
+    bool risingEdge(){ 
         bool rise = _rise;
         _rise = false;
         return rise;
     }
-    void rise(){
+    void rise(){ // set flag        
         _rise = true;
     }
     void fall(){
@@ -35,23 +37,26 @@ class GateInputs
     public:
     GateInputs() : 
         _gates({AUDIO_GATE_1,AUDIO_GATE_2,AUDIO_GATE_3,AUDIO_GATE_4}), 
-        _gateObservers({0,1,2,3}), 
-        _isRising{false,false,false,false}
+        _gateObservers({0,1,2,3}),          // static gate inputs
+        _isRising{false,false,false,false}  // rising edge flags
     {
         pinMode(AUDIO_GATE_1,INPUT_PULLDOWN);
         pinMode(AUDIO_GATE_2,INPUT_PULLDOWN);
         pinMode(AUDIO_GATE_3,INPUT_PULLDOWN);
         pinMode(AUDIO_GATE_4,INPUT_PULLDOWN);
         for(int i = 0; i < 4; i++){
-            _gates[i].attach(&_gateObservers[i]);
+            _gates[i].attach(&_gateObservers[i]); // attach gate observers to gate inputs
         }
     }
+    // read: read gate by channel index
     bool read(int channel){
         return _gates[channel].read();
     }
+    // risingEdge: return true if edge is rising, false is nothing happened
     bool risingEdge(int channel){
         return _gateObservers[channel].risingEdge();
     }
+    // tick: call tick method of all gate observers (IEdegeObserver)
     void tick(){
         for(int i = 0; i < 4; i++){
             _gateObservers[i].tick();
