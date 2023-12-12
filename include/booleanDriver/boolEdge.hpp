ae783cde39e6a20d4722bb46dd1d9608dc25b10c
SImon <33484974+simonjuha@users.noreply.github.com>
2023-12-12 11:46:57 +0100

    boolEdge comments

--- include/booleanDriver/boolEdge.hpp
+++ include/booleanDriver/boolEdge.hpp
@@ -7,6 +7,7 @@ class IEdgeObserver{
     public:
         virtual void rise() = 0;
         virtual void fall() = 0;
+        // checking if the interrupt flag is set and calling the corresponding method
         void tick(){
             if(_interruptRise){
                 rise();
@@ -17,6 +18,7 @@ class IEdgeObserver{
                 _interruptFall = false;
             }
         }
+        // interrupt methods are called by the subject (gpioDriver calls these methods through an interrupt, thus only setting a flag)
         void interruptRise(){
             _interruptRise = true;
         }
@@ -39,19 +41,21 @@ class EdgeSubject{
 };
 
 void EdgeSubject::attach(IEdgeObserver* o){
-    observers.push_back(o);
+    observers.push_back(o); // add observer to vector
 }
 
 void EdgeSubject::detach(IEdgeObserver* o){
-    observers.erase(std::remove(observers.begin(), observers.end(), o), observers.end());
+    observers.erase(std::remove(observers.begin(), observers.end(), o), observers.end()); // remove observer from vector
 }
 
+// sett all observers interrupt flags (rise)
 void EdgeSubject::rise(){
     for(auto o : observers){
         o->interruptRise();
     }
 }
 
+// set all observers interrupt flags (fall)
 void EdgeSubject::fall(){
     for(auto o : observers){
         o->interruptFall();
