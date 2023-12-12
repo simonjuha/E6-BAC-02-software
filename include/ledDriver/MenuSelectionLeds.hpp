32cd22a2bb07054667eb2be54c4c4f53ba9c15b1
SImon <33484974+simonjuha@users.noreply.github.com>
2023-12-12 12:43:43 +0100

    ledDriver comments

--- include/ledDriver/MenuSelectionLeds.hpp
+++ include/ledDriver/MenuSelectionLeds.hpp
@@ -4,20 +4,20 @@
 class MenuSelectionLeds
 {
     public:
-    MenuSelectionLeds() : _leds {new LedDriver(45), new LedDriver(42), new LedDriver(38), new LedDriver(47), new LedDriver(21)}{
+    MenuSelectionLeds() : _leds {new LedDriver(45), new LedDriver(42), new LedDriver(38), new LedDriver(47), new LedDriver(21)}{ // setting up static leds
     }
     void select(int channel){
         for(int i = 0; i < 5; i++){
             if(i == channel){
-                _leds[i]->set(true);
+                _leds[i]->set(true); // set selected led to true
             }else{
-                _leds[i]->set(false);
+                _leds[i]->set(false); // if not selected, turn off
             }
         }
     }
     ILed* getLed(int channel){
-        return _leds[channel];
+        return _leds[channel]; // return led by channel index
     }
     private:
-    ILed *_leds[5];
+    ILed *_leds[5]; // array of leds objects
 };
--- include/ledDriver/ledDriver.hpp
+++ include/ledDriver/ledDriver.hpp
@@ -13,24 +13,24 @@ class LedDriver : public ILed{
     LedDriver(int pinNum){
         _pinNum = static_cast<gpio_num_t>(pinNum);
         gpio_config_t pin_config;
-        pin_config.pin_bit_mask = (1ULL<<_pinNum);
-        pin_config.mode = GPIO_MODE_OUTPUT;
+        pin_config.pin_bit_mask = (1ULL<<_pinNum); // set by bit mask
+        pin_config.mode = GPIO_MODE_OUTPUT; // set as output mode
         pin_config.pull_up_en = GPIO_PULLUP_DISABLE;
         pin_config.pull_down_en = GPIO_PULLDOWN_DISABLE;
-        pin_config.intr_type = GPIO_INTR_DISABLE;
-        gpio_config(&pin_config);
+        pin_config.intr_type = GPIO_INTR_DISABLE; // no interrupt
+        gpio_config(&pin_config); // config the pin
     }
     void set(bool state){
         if(state){
-            gpio_set_level(_pinNum, 1);
-            _state = true;
+            gpio_set_level(_pinNum, 1); // set HIGH
+            _state = true;              // memorize state
         }else{
-            gpio_set_level(_pinNum, 0);
-            _state = false;
+            gpio_set_level(_pinNum, 0); // set LOW
+            _state = false;             // memorize state
         }
     }
     bool state(){
-        return _state;
+        return _state; // return led state
     }
     private:
     gpio_num_t _pinNum;
