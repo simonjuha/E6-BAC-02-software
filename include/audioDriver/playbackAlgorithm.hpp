19db4695d17ca8b85b43697ba04406fae8394e3f
SImon <33484974+simonjuha@users.noreply.github.com>
2023-12-12 11:38:53 +0100

    algorithm comments

--- include/audioDriver/playbackAlgorithm.hpp
+++ include/audioDriver/playbackAlgorithm.hpp
@@ -6,9 +6,9 @@
 class playbackAlgorithm
 {
     public:
-    virtual int16_t & play(Sample &sample) = 0;
-    virtual bool isAtEnd(Sample &sample) = 0;
-    virtual void reset(Sample &sample) = 0;
+    virtual int16_t & play(Sample &sample) = 0; // returns the next sample
+    virtual bool isAtEnd(Sample &sample) = 0;   // returns true if the end of the sample is reached
+    virtual void reset(Sample &sample) = 0;     // resets the algorithm to the beginning of the sample
     protected: 
     unsigned int currentPosition = 0;
 };
@@ -24,7 +24,7 @@ class forwardAlgorithm : public playbackAlgorithm
         return sample.buffer[++currentPosition];
     }
     bool isAtEnd(Sample &sample){
-        return (currentPosition >= sample.size-1);
+        return (currentPosition >= sample.size-1); // end of sample
     }
     void reset(Sample &sample){
         currentPosition = 0;
@@ -37,13 +37,13 @@ class backwardAlgorithm : public playbackAlgorithm
     unsigned int currentPosition = 0;
     int16_t & play(Sample &sample)
     {
-        if(isAtEnd(sample)){
-            currentPosition = sample.size-1;
+        if(isAtEnd(sample)){                        // is at beginning of sample
+            currentPosition = sample.size-1;        // set position to end of sample
         }
-        return sample.buffer[--currentPosition];
+        return sample.buffer[--currentPosition];    // return sample at current position and decrement position
     }
     bool isAtEnd(Sample &sample){
-        return (currentPosition <= 0);
+        return (currentPosition <= 0);  // beginning of sample
     }
     void reset(Sample &sample){
         currentPosition = sample.size-1;
@@ -56,32 +56,33 @@ class pingPongAlgorithm : public playbackAlgorithm
     unsigned int currentPosition = 0;
     int16_t & play(Sample &sample)
     {
-        if(currentPosition >= sample.size-1){
-            currentPosition = sample.size-1;
-            direction = false;
+        if(currentPosition >= sample.size-1){ // if at end of sample
+            currentPosition = sample.size-1;  
+            direction = false; // change direction
         }
-        if(currentPosition <= 0){
+        if(currentPosition <= 0){ // if at beginning of sample
             currentPosition = 0;
-            direction = true;
+            direction = true; // change direction
         }
         if(direction){
-            return sample.buffer[++currentPosition];
+            return sample.buffer[++currentPosition]; // forward
         }else{
-            return sample.buffer[--currentPosition];
+            return sample.buffer[--currentPosition]; // backward
         }
     }
+    // isAtEnd: is not use internally, but used by the audioDrier when in TRIGGER_ONCE_MODE 
     bool isAtEnd(Sample &sample){
-        return (currentPosition <= 0) || (currentPosition >= sample.size-1);
+        return (currentPosition <= 0) || (currentPosition >= sample.size-1); // beginning or end of sample
     }
     void reset(Sample &sample){
-        currentPosition = (sample.size-1) / 2;
+        currentPosition = (sample.size-1) / 2; // set position to middle of sample on reset
     }
     private:
     bool direction = true;
 };
 
 // play small chunks of the sample
-// finds new random position after grain is played
+// finds new random position after grain is played (forward)
 class granularAlgorithm : public playbackAlgorithm{
     public:
     int16_t & play(Sample &sample){
@@ -90,19 +91,20 @@ class granularAlgorithm : public playbackAlgorithm{
         }
         return sample.buffer[grainStartPositions + currentGrainPosition++];
     }
+    // isAtEnd: in this algorith isAtEnd is used to find the end of the grain and not the whole sample
     bool isAtEnd(Sample &sample){
-        bool end = (currentGrainPosition >= grainSize);
+        bool end = (currentGrainPosition >= grainSize); // end of grain
         if(end){
             reset(sample);
         }
         return end;
     }
     void reset(Sample &sample){
-        grainSize = sample.size / 8;
+        grainSize = sample.size / 8; // grainsize is currently 1/8 of the sample size
         if(sample.size > grainSize){
-            grainStartPositions = random(0, sample.size - grainSize);
+            grainStartPositions = random(0, sample.size - grainSize); // get random start position
         } else {
-            grainStartPositions = 0;
+            grainStartPositions = 0; // if sample is smaller than grainsize set start position to 0
         }
         currentGrainPosition = 0;
     }
